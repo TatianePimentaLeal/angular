ae76f148a689d75b36b87d8bfa2abc7088173628
TatyPimentaLeal <tatyplummer07@gmail.com>
2023-07-26 05:15:59 -0300

    Utilizando o debounce do rxjs

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -9,21 +10,22 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>;
   
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) {} //vamos usar o constructor apenas para injeção de dependência
+  constructor(private activatedRoute: ActivatedRoute) { } //vamos usar o constructor apenas para injeção de dependência
   
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName;
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter);
   }
-
+  
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe;
+  }
 }
